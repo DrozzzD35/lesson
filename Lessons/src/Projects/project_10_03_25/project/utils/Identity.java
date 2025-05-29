662eab51aa6746a8a8b3c775f22231a2bf753016
DrozzzD35 <kapustin3595@yandex.ru>
2025-05-29 11:48:51 +0300

    пройден урок

--- Lessons/src/Projects/project_10_03_25/project/utils/Identity.java
+++ Lessons/src/Projects/project_10_03_25/project/utils/Identity.java
@@ -7,7 +7,6 @@ public enum Identity {
     INSTANCE;
     private int identifier = 0;
 
-    //  todo написать логику создание и обновление id для новых тасков
     public int generateId() {
         return identifier++;
     }
