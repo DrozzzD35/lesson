8b8bf43016a0be390e35981b5baa82874f02ae0c
DrozzzD35 <kapustin3595@yandex.ru>
2025-11-04 14:04:38 +0300

    Решить таски с литкода

--- Lessons/src/leetCode/Task_01_11_2025/Task2/Solution.java
+++ Lessons/src/leetCode/Task_01_11_2025/Task2/Solution.java
@@ -2,9 +2,23 @@ package leetCode.Task_01_11_2025.Task2;
 
 public class Solution {
     public int lengthOfLastWord(String s) {
+        String string = s.trim();
+        int a = 0;
+        int j = string.length();
 
+        for (int i = string.length() - 1; i >= 0; i--) {
 
+            if (string.charAt(i) == ' ') {
+                a = i + 1;
+                break;
 
+            } else {
+                a = i;
+            }
+
+        }
+
+        return j - a;
 
     }
 
