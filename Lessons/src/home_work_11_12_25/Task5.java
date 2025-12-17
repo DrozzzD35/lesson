70a686060325abbb8e5a4f810f7b37f9a0abd1a4
DrozzzD35 <kapustin3595@yandex.ru>
2025-12-17 06:59:54 +0300

    Решить задачи

--- Lessons/src/home_work_11_12_25/Task5.java
+++ Lessons/src/home_work_11_12_25/Task5.java
@@ -7,7 +7,11 @@ import java.util.List;
 public class Task5 {
 
     public static void main(String[] args) {
+        int[] nums = {-1, 0, 1, 2, -1, -4};
 
+        System.out.println(threeSum(nums));
+
+        System.out.println(threeSum2(nums));
     }
 
     static List<List<Integer>> threeSum(int[] nums) {
@@ -18,9 +22,17 @@ public class Task5 {
         int n = nums.length;
         int sum;
 
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n - 2; i++) {
             left = i + 1;
             right = n - 1;
+
+            if (nums[i] > 0) {
+                break;
+            }
+
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
 
             while (left < right) {
                 sum = nums[left] + nums[right] + nums[i];
@@ -32,10 +44,52 @@ public class Task5 {
                     lists.add(List.of(nums[left], nums[right], nums[i]));
                     left++;
                     right--;
+
+                    while (left < right && nums[left] == nums[left - 1]) {
+                        left++;
+                    }
+
+                    while (left < right && nums[right] == nums[right + 1]) {
+                        right--;
+                    }
+
                 }
             }
         }
 
         return lists;
     }
+
+    static List<List<Integer>> threeSum2(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        Arrays.sort(nums);
+
+        for (int i = 0; i < nums.length; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+
+            int j = i + 1;
+            int k = nums.length - 1;
+
+            while (j < k) {
+                int total = nums[i] + nums[j] + nums[k];
+
+                if (total > 0) {
+                    k--;
+                } else if (total < 0) {
+                    j++;
+                } else {
+                    res.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                    j++;
+
+                    while (nums[j] == nums[j - 1] && j < k) {
+                        j++;
+                    }
+                }
+            }
+        }
+        return res;
+    }
 }
+
