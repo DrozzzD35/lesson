016a3efebbac15caed970f00024d09f4edd443f4
DrozzzD35 <kapustin3595@yandex.ru>
2025-12-20 04:49:34 +0300

    Решить задачи

---
+++ Lessons/src/home_work_11_12_25/Task6.java
@@ -0,0 +1,59 @@
+package home_work_11_12_25;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Task6 {
+    public static void main(String[] args) {
+        int[] nums = {2, 0, 2, 1, 1, 0};
+
+        sortColors2(nums);
+        System.out.println(Arrays.toString(nums));
+
+    }
+
+    public static void sortColors2(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int j = 0;
+        int index = 0;
+
+        if (nums.length == 1) {
+            return;
+        }
+
+        while (j < nums.length) {
+            map.put(nums[j], map.getOrDefault(nums[j], 0) + 1);
+            j++;
+        }
+
+        for (int collor = 0; collor <= 2; collor++) {
+            int value = map.getOrDefault(collor, 0);
+
+            while (value > 0) {
+                nums[index] = collor;
+                index++;
+                value--;
+            }
+        }
+
+
+    }
+
+    public static void sortColors(int[] nums) {
+        int[] counts = new int[3];
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 0) {
+                counts[0] += 1;
+            } else if (nums[i] == 1) {
+                counts[1] += 1;
+            } else {
+                counts[2] += 1;
+            }
+        }
+
+    }
+
+
+}
